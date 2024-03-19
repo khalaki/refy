cd8e0054a3e1a077d3ce3a347b1b69afd0fd8939
Ivan Khalak <halakivan@gmail.com>
2024-03-19 22:13:24 +0100

    init commit

---
+++ bot.py
@@ -0,0 +1,108 @@
+import telebot
+import config
+from telebot import types
+import os
+from webui import StableDiffusionAPI, resize_image_to_max_dimension, load_and_prepare_payload
+
+bot = telebot.TeleBot(config.TOKEN)
+api_client = StableDiffusionAPI('http://172.20.176.1:7860', 'api_out')
+
+users_process_data = {}
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+    markup.add('Exterior', 'Interior')
+    msg = bot.send_message(message.chat.id, f"Hello, {message.from_user.first_name}! Let's start! 💫\nPlease select the type of your base image.", reply_markup=markup)
+    bot.register_next_step_handler(msg, process_image_type_step)
+
+def process_image_type_step(message):
+    chat_id = message.chat.id
+    users_process_data[chat_id] = {'image_type': message.text}
+    if message.text == 'Exterior':
+        msg = bot.send_message(chat_id, "Please upload a photo of the exterior.")
+        bot.register_next_step_handler(msg, process_photo_upload)
+    elif message.text == 'Interior':
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+        markup.add('Kitchen', 'Living Room', 'Bathroom')  # Add more options as needed
+        msg = bot.send_message(chat_id, "What type of room are you interested in?", reply_markup=markup)
+        bot.register_next_step_handler(msg, process_room_type_step)
+
+def process_room_type_step(message):
+    chat_id = message.chat.id
+    users_process_data[chat_id]['room_type'] = message.text  # This could be used for further customization
+    msg = bot.send_message(chat_id, "Please upload a photo of the interior.")
+    bot.register_next_step_handler(msg, process_photo_upload)
+
+def process_photo_upload(message):
+    chat_id = message.chat.id
+    if not message.photo:
+        msg = bot.reply_to(message, "This does not seem to be an image. Please upload an image.")
+        bot.register_next_step_handler(msg, process_photo_upload)
+        return
+    
+    file_id = message.photo[-1].file_id
+    file_info = bot.get_file(file_id)
+    downloaded_file = bot.download_file(file_info.file_path)
+    
+    image_path = f'temp/{chat_id}_image.jpg'
+    with open(image_path, 'wb') as new_file:
+        new_file.write(downloaded_file)
+    
+    users_process_data[chat_id]['image_path'] = image_path
+    msg = bot.send_message(chat_id, "Now, please upload a reference image.")
+    bot.register_next_step_handler(msg, process_reference_upload)
+
+def process_reference_upload(message):
+    chat_id = message.chat.id
+    if not message.photo:
+        msg = bot.reply_to(message, "This does not seem to be an image. Please upload an image.")
+        bot.register_next_step_handler(msg, process_reference_upload)
+        return
+    
+    file_id = message.photo[-1].file_id
+    file_info = bot.get_file(file_id)
+    downloaded_file = bot.download_file(file_info.file_path)
+    
+    ref_image_path = f'temp/{chat_id}_ref_image.jpg'
+    with open(ref_image_path, 'wb') as new_file:
+        new_file.write(downloaded_file)
+    bot.reply_to(message, "Thank you! Processing your request...")
+    process_images_and_send(chat_id, users_process_data[chat_id]['image_path'], ref_image_path)
+
+def process_images_and_send(chat_id, image_path, ref_image_path):
+    # This is a simplified version; you may want to handle this asynchronously in production
+
+    webui_server_url = 'http://172.20.176.1:7860'
+    output_dir = 'api_out'
+    api_client = StableDiffusionAPI(webui_server_url, output_dir)
+    payload_file_path = 'payload.json'
+
+    payload = load_and_prepare_payload(payload_file_path, image_path, ref_image_path)
+    api_client.call_img2img_api(**payload)
+    
+    
+    # Identify all PNG images in the output directory
+    image_files = [f for f in os.listdir(api_client.out_dir_i2i) if f.endswith('.png')]
+    # Sort the files to ensure they are processed in a meaningful order
+    sorted_image_files = sorted(image_files, key=lambda x: (int(x.split('-')[-1].split('.')[0]) if x.split('-')[-1].split('.')[0].isdigit() else float('inf'), x))
+
+    # Send only the first four processed images back to the user
+    for img_file in sorted_image_files[:4]:
+        img_path = os.path.join(api_client.out_dir_i2i, img_file)
+        with open(img_path, 'rb') as photo:
+            bot.send_photo(chat_id, photo)
+    
+    # Cleanup: Remove all images in the output directory after sending the first four
+    for img_file in os.listdir(api_client.out_dir_i2i):
+        os.remove(os.path.join(api_client.out_dir_i2i, img_file))
+
+    # Also remove the uploaded images to prevent storage overflow
+    os.remove(image_path)
+    os.remove(ref_image_path)
+
+
+if __name__ == '__main__':
+    if not os.path.exists('temp'):
+        os.makedirs('temp')
+    bot.polling(non_stop=True)
---
+++ webui.py
@@ -0,0 +1,98 @@
+from datetime import datetime
+import urllib.request
+import base64
+import json
+import time
+import os
+from PIL import Image
+
+class StableDiffusionAPI:
+    def __init__(self, server_url, output_directory):
+        self.server_url = server_url
+        self.output_directory = output_directory
+        self.out_dir_t2i = os.path.join(output_directory, 'txt2img')
+        self.out_dir_i2i = os.path.join(output_directory, 'img2img')
+        self.ensure_directories()
+
+    def ensure_directories(self):
+        os.makedirs(self.out_dir_t2i, exist_ok=True)
+        os.makedirs(self.out_dir_i2i, exist_ok=True)
+
+    @staticmethod
+    def timestamp():
+        return datetime.fromtimestamp(time.time()).strftime("%Y%m%d-%H%M%S")
+
+    @staticmethod
+    def encode_file_to_base64(path):
+        with open(path, 'rb') as file:
+            return base64.b64encode(file.read()).decode('utf-8')
+
+    @staticmethod
+    def decode_and_save_base64(base64_str, save_path):
+        with open(save_path, "wb") as file:
+            file.write(base64.b64decode(base64_str))
+
+    def call_api(self, api_endpoint, **payload):
+        data = json.dumps(payload).encode('utf-8')
+        request = urllib.request.Request(
+            f'{self.server_url}/{api_endpoint}',
+            headers={'Content-Type': 'application/json'},
+            data=data,
+        )
+        response = urllib.request.urlopen(request)
+        return json.loads(response.read().decode('utf-8'))
+
+    def call_txt2img_api(self, **payload):
+        response = self.call_api('sdapi/v1/txt2img', **payload)
+        for index, image in enumerate(response.get('images')):
+            save_path = os.path.join(self.out_dir_t2i, f'txt2img-{self.timestamp()}-{index}.png')
+            self.decode_and_save_base64(image, save_path)
+
+    def call_img2img_api(self, **payload):
+        response = self.call_api('sdapi/v1/img2img', **payload)
+        for index, image in enumerate(response.get('images')):
+            save_path = os.path.join(self.out_dir_i2i, f'img2img-{self.timestamp()}-{index}.png')
+            self.decode_and_save_base64(image, save_path)
+
+def resize_image_to_max_dimension(image_path, max_dimension=768):
+    with Image.open(image_path) as img:
+        original_width, original_height = img.size
+        scaling_factor = max_dimension / max(original_width, original_height)
+        return int(original_width * scaling_factor), int(original_height * scaling_factor)
+
+def load_and_prepare_payload(file_path, image_path, ref_image_path):
+    with open(file_path, 'r') as file:
+        payload = json.load(file)
+
+    new_width, new_height = resize_image_to_max_dimension(image_path)
+    encoded_ref_image = StableDiffusionAPI.encode_file_to_base64(ref_image_path)
+    payload['init_images'] = [StableDiffusionAPI.encode_file_to_base64(image_path)]
+    payload['height'] = new_height
+    payload['width'] = new_width
+    payload['batch_size'] = 1
+    payload['alwayson_scripts']['ControlNet']['args'][1]['image']['image'] = encoded_ref_image
+    payload['alwayson_scripts']['ControlNet']['args'][1]['image']['mask'] = None
+
+    return payload
+
+def main():
+    webui_server_url = 'http://172.20.176.1:7860'
+    output_dir = 'api_out'
+    api_client = StableDiffusionAPI(webui_server_url, output_dir)
+
+    payload_file_path = 'payload.json'
+    image_path = "/mnt/c/Users/Anastasiia/Downloads/Telegram Desktop/doors.interior.jpg"
+    ref_image_path = "/mnt/c/Users/Anastasiia/Downloads/Telegram Desktop/2.jpg"
+
+    payload = load_and_prepare_payload(payload_file_path,image_path,ref_image_path)
+
+    ### debug mode
+    output_payload_file_path = 'payload_output.json'
+    with open(output_payload_file_path, 'w') as file:
+        json.dump(payload, file, indent=4)
+    ###
+
+    api_client.call_img2img_api(**payload)
+
+if __name__ == '__main__':
+    main()
