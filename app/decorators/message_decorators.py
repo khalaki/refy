c913d7a77df18f3367a4b875d32009729e059d11
Ivan Khalak <Ivan.Khalak@dkv-mobility.com>
2024-06-06 22:45:06 +0200

    start from begining is always available now

---
+++ app/decorators/message_decorators.py
@@ -0,0 +1,35 @@
+from telebot.handler_backends import State, StatesGroup
+
+from utils.bot import telegram
+from handlers import init_handlers
+
+class UserStates(StatesGroup):
+    START = State()
+    IMAGE_TYPE = State()
+    PHOTO_UPLOAD = State()
+    REFERENCE_UPLOAD = State()
+
+def error_handler(func):
+    def wrapper(message, *args, **kwargs):
+        try:
+            return func(message, *args, **kwargs)
+        except Exception as e:
+            telegram.send_message(message.chat.id, 'An error occurred. Please try again later.')
+            # Log the error
+            print(f"Error: {e}")
+    return wrapper
+
+def start_command_handler(func):
+    def wrapper(message, *args, **kwargs):
+        if message.text == '/start':
+            init_handlers.send_welcome(message)
+            return
+        else:
+            return func(message, *args, **kwargs)
+    return wrapper
+
+def log_message_handler(func):
+    def wrapper(message, *args, **kwargs):
+        print(f"Handling message from {message.chat.id}: {message.text}")
+        return func(message, *args, **kwargs)
+    return wrapper
--- app/handlers/photo_handlers.py
+++ app/handlers/photo_handlers.py
@@ -1,6 +1,38 @@
+from telebot import types
+
 from .processing import process_images_and_send
 from utils.bot import telegram, users_process_data
+from decorators import start_command_handler, log_message_handler, error_handler
 
+@start_command_handler
+@log_message_handler
+@error_handler
+def process_image_type_step(message):
+  chat_id = message.chat.id
+  users_process_data[chat_id] = {'image_type': message.text}
+
+  if message.text == 'Exterior':
+    msg = telegram.send_message(chat_id, "Please upload a photo of the exterior.")
+    telegram.register_next_step_handler(msg, process_photo_upload)
+  elif message.text == 'Interior':
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+    markup.add('Kitchen', 'Living Room', 'Bathroom')  # Add more options as needed
+    msg = telegram.send_message(chat_id, "What type of room are you interested in?", reply_markup=markup)
+    telegram.register_next_step_handler(msg, process_room_type_step)
+
+@start_command_handler
+@log_message_handler
+@error_handler
+def process_room_type_step(message):
+  chat_id = message.chat.id
+
+  users_process_data[chat_id]['room_type'] = message.text  # This could be used for further customization
+  msg = telegram.send_message(chat_id, "Please upload a photo of the interior.")
+  telegram.register_next_step_handler(msg, process_photo_upload)
+
+@start_command_handler
+@log_message_handler
+@error_handler
 def process_photo_upload(message):
     chat_id = message.chat.id
 
@@ -21,6 +53,9 @@ def process_photo_upload(message):
     msg = telegram.send_message(chat_id, "Now, please upload a reference image.")
     telegram.register_next_step_handler(msg, process_reference_upload)
 
+@start_command_handler
+@log_message_handler
+@error_handler
 def process_reference_upload(message):
     chat_id = message.chat.id
     if not message.photo:
